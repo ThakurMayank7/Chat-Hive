a426b1245af16de5da0df79fe251dfc0007955e2
Mayank <mayanksingh8181335335@gmail.com>
2025-01-23 03:16:23 +0530

    made some updates

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,9 @@ import { Separator } from "./ui/separator";
 import { ChatPreviewDetails, FirebaseUser, UserData } from "@/lib/types";
 import { ThreeDotsSpinner } from "./Spinners";
 import ChatPreview from "./ChatPreview";
+import { ScrollArea } from "./ui/scroll-area";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface SidebarProps {
   syncState: boolean;
@@ -14,33 +17,57 @@ interface SidebarProps {
   loading: boolean;
 }
 
-function Sidebar({ syncState, userData }: SidebarProps) {
+function Sidebar({ syncState, userData, user }: SidebarProps) {
   return (
     <div className="w-80 flex flex-col border-r-2">
-      <div className="flex items-center justify-center h-16">
-        <Logo />
-      </div>
-      <Separator />
-      {userData?.name}
-      {syncState && <ThreeDotsSpinner />}
-      {!syncState &&
-        userData &&
-        userData.chats.map((chat: string) => {
-          return (
-            <ChatPreview
-              chatPreview={
-                {
-                  chatId: chat,
-                  name: "",
-                  lastMessage: "",
-                  lastMessageAt: "",
-                  type: "private",
-                } as ChatPreviewDetails
-              }
-              key={chat}
-            />
-          );
-        })}
+      <ScrollArea className="h-full w-full">
+        <div className="sticky top-0 z-10 flex flex-col py-2">
+          <div className="flex items-center justify-center h-16">
+            <Logo />
+          </div>
+
+          <Separator />
+          <div className="flex flex-row p-2">
+            <Avatar className="border-2 border-black">
+              <AvatarImage
+                src={`${
+                  user.photoURL
+                    ? user.photoURL
+                    : "https://github.com/shadcn.png"
+                }
+                `}
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col items-center justify-center ml-2">
+              <span className="text-md font-serif">{user.displayName}</span>
+            </div>
+            <div className="ml-auto">
+              <AiOutlinePlusCircle size={42} color="teal" />
+            </div>
+          </div>
+          <Separator />
+        </div>
+        {syncState && <ThreeDotsSpinner />}
+        {!syncState &&
+          userData &&
+          userData.chats.map((chat: string) => {
+            return (
+              <ChatPreview
+                chatPreview={
+                  {
+                    chatId: chat,
+                    name: "",
+                    lastMessage: "",
+                    lastMessageAt: "",
+                    type: "private",
+                  } as ChatPreviewDetails
+                }
+                key={chat}
+              />
+            );
+          })}
+      </ScrollArea>
     </div>
   );
 }
